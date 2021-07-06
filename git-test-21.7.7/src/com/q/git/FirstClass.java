b38ab929f3e92a48fb2b5dc78c37ff0d20c544de
Sirius <1206207054@qq.com>
2021-07-07 00:42:14 +0800

    commit2

--- git-test-21.7.7/src/com/q/git/FirstClass.java
+++ git-test-21.7.7/src/com/q/git/FirstClass.java
@@ -3,5 +3,9 @@ package com.q.git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git");
+
+        for(int i=0;i<100;i++){
+            System.out.println(i);
+        }
     }
 }
